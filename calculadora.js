3b09ff5774f3a2f07c2e1020ad0207d28808f9b3
matheus <sartorhenn@gmail.com>
2026-05-26 15:13:28 -0300

    feature

--- calculadora.js
+++ calculadora.js
@@ -53,3 +53,62 @@ function calculateDiscount(price, discount) {
 const price = 100;
 const discount = 10;
 console.log("Preço final com desconto: " + calculateDiscount(price, discount));
+
+const matrizGastos = [
+   ["Alimentação",0],
+   ["Transporte",0],
+   ["Lazer",0],
+   ["Total",0],
+   
+]
+
+const atualizaValorCategoria = (categoria,valor) => categoria[1] = somaValor(categoria[1],valor);
+const obterElemento = (id) => document.getElementById(id);
+const obterValor = () => parseFloat(obterElemento("valor").value);
+const obterCategoriaInformada = () => obterElemento("categoria").value;
+const valorNegativo = (valor) => valor < 0;
+const somaValor = (total, valor) => total + valor;
+const obeterCategoria = (matriz, nomeCategoria) => matriz.find((item) => item [0] === nomeCategoria);
+
+const atualizarInterface = () => {
+
+   matrizGastos.forEach(([nome, valor]) => {
+      const elemento = obterElemento(nome);
+      elemento.textContent = `${nome} : R$ ${valor}`
+   })
+}
+
+const limparCampos = () => obterElemento("valor").value = '';
+
+function adicionarGasto()
+{
+
+   const valorInformado = obterValor();
+   const categoriaInformada = obterCategoriaInformada();
+
+   if(valorNegativo(valorInformado)){
+      alert ("Valor inválido");
+      return;
+   }
+
+const categoria = obeterCategoria(matrizGastos, categoriaInformada);
+
+atualizaValorCategoria(categoria, valorInformado);
+
+const total = obeterCategoria (matrizGastos, "Total");
+
+atualizaValorCategoria(categoria, valorInformado);
+atualizaValorCategoria(total, valorInformado);
+atualizarInterface();
+limparCampos();   
+}
+
+function calculateMovieDuration(minutes) {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return hours + "h " + remainingMinutes + "m";
+}
+
+const movieDuration = calculateMovieDuration(125);
+console.log("Duração do filme: " + movieDuration);
+    
